a17ceeb75a3530a10c97568cc8d8f2c3d8edf0e8
Yasequeponer <diegosarango.garcia@gmail.com>
2025-11-02 20:45:48 +0100

    Se han comentado los archivos products.phtml y se han añadido algunos comentarios a run.php

--- run.php
+++ run.php
@@ -1,9 +1,12 @@
 <?php
-
-
+///@date 02-11-2025
+///Reporta todos los errores
 error_reporting(E_ALL);
+
+///Muestra los errores en pantalla
 ini_set('display_errors', 1);
 
+///Se requieren los archivos "config.php" y "functions.php"
 require __DIR__ . '/includes/config.php';
 require __DIR__ . '/includes/functions.php';
 
